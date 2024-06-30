64d0d6d7886bc6f2ffbdfa9d12d5a6c0e7e748f4
Anubhav <121851863+Anubhavprogramer@users.noreply.github.com>
2024-06-30 21:18:03 +0530

    updates

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     horizontal_scroll_animation();
     color_changer();
     navelement();
-    swiper();
+    now_trending_slider();
 });
 
 
@@ -52,21 +52,115 @@ function navelement() {
     }
 )}
 
-function swiper(){
-    var swiper = new Swiper(".mySwiper", {
-        spaceBetween: 30,
-        centeredSlides: true,
-        autoplay: {
-          delay: 5000,
-          disableOnInteraction: false,
-        },
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      });
+
+class DragScroll {
+    constructor(obj) {
+        this.el = document.querySelector(obj.el);
+        this.wrap = document.querySelector(obj.wrap);
+        this.items = document.querySelectorAll(obj.item);
+        this.init();
+    }
+
+    init() {
+        this.progress = 0;
+        this.speed = 0;
+        this.oldX = 0;
+        this.x = 0;
+        this.playrate = 0;
+        this.dragging = false;
+        this.startX = 0;
+
+        this.bindings();
+        this.events();
+        this.calculate();
+        this.raf();
+    }
+
+    bindings() {
+        [
+            'events',
+            'calculate',
+            'raf',
+            "handleWheel",
+            "move",
+            "handleTouchStart",
+            "handleTouchMove",
+            "handleTouchEnd",
+        ].forEach(fn => this[fn] = this[fn].bind(this));
+    }
+
+    calculate() {
+        this.progress = 0;
+        this.wrapWidth = Array.from(this.items).reduce((total, item) => total + item.clientWidth, 0);
+        this.maxScroll = this.wrapWidth - this.el.clientWidth;
+    }
+
+    handleWheel(e) {
+        this.progress += e.deltaY;
+        this.move();
+    }
+
+    handleTouchStart(e) {
+        e.preventDefault();
+        this.dragging = true;
+        this.startX = e.clientX || e.touches[0].clientX;
+    }
+
+    handleTouchMove(e) {
+        if (!this.dragging) return;
+
+        const x = e.clientX || e.touches[0].clientX;
+        this.progress += (this.startX - x) * 2.5;
+        this.startX = x;
+        this.move();
+    }
+
+    handleTouchEnd() {
+        this.dragging = false;
+    }
+
+    move() {
+        this.progress = Math.max(0, Math.min(this.progress, this.maxScroll));
+    }
+
+    events() {
+        window.addEventListener('resize', this.calculate);
+        window.addEventListener('wheel', this.handleWheel);
+
+        this.el.addEventListener("touchstart", this.handleTouchStart);
+        window.addEventListener("touchmove", this.handleTouchMove);
+        window.addEventListener("touchend", this.handleTouchEnd);
+
+        window.addEventListener("mousedown", this.handleTouchStart);
+        window.addEventListener("mousemove", this.handleTouchMove);
+        window.addEventListener("mouseup", this.handleTouchEnd);
+
+        document.addEventListener("mouseleave", this.handleTouchEnd);
+    }
+
+    raf() {
+        this.x = this.x + (this.progress - this.x) * 0.1;
+        this.playrate = this.x / this.maxScroll;
+        this.wrap.style.transform = `translateX(${-this.x}px)`;
+
+        requestAnimationFrame(this.raf);
+    }
 }
+
+function now_trending_slider(){
+    const scroll = new DragScroll({
+        el: ".slider",
+        wrap: ".slider-wrapper",
+        item: ".slider-item",
+    });
+    
+    const animateScroll = () => {
+        requestAnimationFrame(animateScroll);
+        scroll.raf();
+    };
+    
+    animateScroll();
+}
+
+
+
